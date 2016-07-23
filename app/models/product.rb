8cb85cf7dce177556185c8d16de1d4bd06371423
fnumono <wangpramono@gmail.com>
2016-07-23 05:32:59 -0700

    cart before ajax and design

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,25 @@
 class Product < ActiveRecord::Base
 
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+
 	def self.search(query)
    		 where("category like ?", "%#{query}%") 
+  	end
+
+
+
+  	private 
+
+  	def ensure_not_referenced_by_any_line_item
+  		if line_items.empty?
+  			return true
+  		else
+  			errors.add(:base, 'Line Item Present')
+  			return false
+  		end
   	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722210552) do
+ActiveRecord::Schema.define(version: 20160723114757) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,22 @@ ActiveRecord::Schema.define(version: 20160722210552) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
+  end
+
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
